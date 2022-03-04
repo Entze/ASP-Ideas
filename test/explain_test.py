442d6919cfd9ba336576822e3ef4901d4f08c674
Lukas Grassauer <lukas@grassauer.eu>
2022-03-04 16:38:01 +0100

    implement Algorithm 2-4

--- test/explain_test.py
+++ test/explain_test.py
@@ -15,55 +15,60 @@ asp 1 0 0
 4 1 f 1 6
 0
 """
-from util.explain import _preprocess, negation_atoms
+from util.explain import _preprocess, negation_atoms, _derivation_path, _dependency_assumption
 
 example_literal_mapping = dict(b=2, k=4, a=3, e=1, c=5, f=6)
 a, b, c, e, f, k = example_literal_mapping['a'], example_literal_mapping['b'], example_literal_mapping['c'], \
                    example_literal_mapping['e'], example_literal_mapping['f'], example_literal_mapping['k']
 
 example_program = {
-    1: [dict()],
-    2: [dict(positive=[], negative=[3])],
-    4: [dict(positive=[1], negative=[2])],
-    3: [dict(positive=[4], negative=[2])],
-    5: [dict(positive=[2, 3], negative=[]), dict(positive=[1, 4], negative=[])],
-    6: [dict(positive=[1], negative=[5, 4])]
+    e: [dict()],
+    b: [dict(positive=[], negative=[a])],
+    k: [dict(positive=[e], negative=[b])],
+    a: [dict(positive=[k], negative=[b])],
+    c: [dict(positive=[a, b], negative=[]), dict(positive=[k], negative=[])],
+    f: [dict(positive=[e], negative=[k, c])]
 }
 example_facts = {e}
 
 example_answer_set = {b, f, e}
+
+example_derivable_dict = {
+    f: [{-k, -c, e}],
+    b: [{-a}],
+    -c: [{-k, -a}],
+    -k: [{b}],
+    -a: [{-k}, {b}],
+    e: [{"T"}]
+}
 
 
 def preprocess_test():
     ep = _preprocess(example_program, example_facts, example_answer_set)
     assert f in ep
     assert len(ep[f]) == 1, f"{len(ep[f])} != 1"
-    assert sorted(ep[f][0]) == sorted({-k, -c, e}), f"{sorted(ep[f][0])} != " + "{" + ", ".join(
-        map(str, sorted((-k, -c, e)))) + "}"
+    assert sorted(ep[f][0]) == sorted({-k, -c, e}), f"{sorted(ep[f][0])} != {sorted({-k, -c, e})}"
 
     assert b in ep
     assert len(ep[b]) == 1, f"{len(ep[b])} != 1"
-    assert sorted(ep[b][0]) == sorted({-a}), f"{sorted(ep[b][0])} != " + "{" + ", ".join(map(str, sorted((-a,)))) + "}"
+    assert sorted(ep[b][0]) == sorted({-a}), f"{sorted(ep[b][0])} != {sorted({-a})}"
 
     assert -c in ep
     assert len(ep[-c]) == 1, f"{ep[-c]} != 1"
-    assert sorted(ep[-c][0]) == sorted({-k, -a}), f"{sorted(ep[-c][0])} != " + "{" + ", ".join(
-        map(str, sorted((-k, -a))))
+    assert sorted(ep[-c][0]) == sorted({-k, -a}), f"{sorted(ep[-c][0])} != {sorted({-k, -a})}"
 
     assert -k in ep
     assert len(ep[-k]) == 1, f"{ep[-k]} != 1"
-    assert sorted(ep[-k][0]) == sorted({b}), f"{sorted(ep[-k][0])} != " + "{" + ", ".join(map(str, sorted((b,)))) + "}"
+    assert sorted(ep[-k][0]) == sorted({b}), f"{sorted(ep[-k][0])} != {sorted({b})}"
 
     assert -a in ep
     assert len(ep[-a]) == 2, f"{len(ep[-a])} != 2"
-    assert sorted(ep[-a][0]) == sorted({-k}), f"{sorted(ep[-a][0])} != " + "{" + ", ".join(
-        map(str, sorted((-k,)))) + "}"
-    assert sorted(ep[-a][1]) == sorted({b}), f"{sorted(ep[-a][1])} != " + "{" + ", ".join(map(str, sorted((b,)))) + "}"
+    assert sorted(ep[-a][0]) == sorted({-k}), f"{sorted(ep[-a][0])} != {sorted({-k})}"
+    assert sorted(ep[-a][1]) == sorted({b}), f"{sorted(ep[-a][1])} != {sorted({b})}"
 
     assert e in ep
     assert len(ep[e]) == 1, f"{len(ep[e])} != 1"
-    assert sorted(ep[e][0]) == sorted({"T"}), f"{sorted(ep[e][0])} != " + "{" + ", ".join(
-        map(str, sorted(("T",)))) + "}"
+    assert sorted(ep[e][0]) == sorted({"T"}), f"{sorted(ep[e][0])} != {sorted({'T'})}"
 
     assert len(ep) == 6, f"{len(ep)} != 6"
 
@@ -73,5 +78,21 @@ def negation_atoms_test():
     assert sorted(nant) == sorted({a, b, c, k}), f"{sorted(nant)} != f{sorted({a, b, c, k})}"
 
 
+def derivation_path_test():
+    TA = {c, a, k}
+    T, DA = _derivation_path(TA, example_derivable_dict)
+    assert not T
+    expected_DA = {a: {k}, k: {a}, c: {k, a}}
+    assert DA == expected_DA, f"{DA} != {expected_DA}"
+
+
+def assumption_func_test():
+    T, DA = set(), {a: {k}, k: {a}, c: {k, a}}
+    D = _dependency_assumption(DA)
+    assert D == {frozenset({a}), frozenset({k})}, f"{D} != {({frozenset({a}), frozenset({k})})}"
+
+
 preprocess_test()
 negation_atoms_test()
+derivation_path_test()
+assumption_func_test()
--- util/explain.py
+++ util/explain.py
@@ -1,9 +1,10 @@
-import operator
+import itertools
 
 
-def _preprocess(program_dict: dict, facts: set, answer_set: set):
+def _preprocess(program_dict: dict, facts: set, answer_set: set) -> dict:
     derivable_dict = dict()
     for head, bodies in program_dict.items():
+        # print(f"[_preprocess]: {head} start.")
         if head in answer_set:
             derivable_dict[head] = []
             for body in bodies:
@@ -14,47 +15,32 @@ def _preprocess(program_dict: dict, facts: set, answer_set: set):
             for body in bodies:
                 _false_atoms_process(head, body.get('positive', list()), body.get('negative', list()),
                                      derivable_dict, answer_set)
+        # print(f"[_preprocess]: {head} done.")
     return derivable_dict
 
 
 def _true_atoms_process(head: int, positive_symbols: list, negative_symbols: list, deriveable_dict: dict,
                         answer_set: set, facts: set):
-    positive_rules_are_subset = True
-    for positive_symbol in positive_symbols:
-        positive_rules_are_subset = positive_rules_are_subset and positive_symbol in answer_set
-        if not positive_rules_are_subset:
-            break
-    negative_rules_are_complement = True
-    for negative_symbol in negative_symbols:
-        negative_rules_are_complement = negative_rules_are_complement and negative_symbol not in answer_set
-        if not negative_rules_are_complement:
-            break
-    if positive_rules_are_subset and negative_rules_are_complement:
-        minimal_set = set(positive_symbols).union(map(operator.neg, negative_symbols))  # TODO: better name
+    if all(p in answer_set for p in positive_symbols) and not any(n in answer_set for n in negative_symbols):
+        derivable = set(positive_symbols) | set(-n for n in negative_symbols)
         if head in facts and not positive_symbols and not negative_symbols:
-            minimal_set = {"T"}
-        deriveable_dict[head].append(minimal_set)
+            derivable = {"T"}
+        deriveable_dict[head].append(derivable)
 
 
 def _false_atoms_process(head: int, positive_rules: list, negative_rules: list, derivable_dict: dict,
                          answer_set: set):
     assert isinstance(derivable_dict, dict)
-    answer_set_complement = []
-    for p in positive_rules:
-        if p not in answer_set:
-            answer_set_complement.append({-p})
-    for n in negative_rules:
-        if n in answer_set and {n} not in answer_set_complement:
-            answer_set_complement.append({n})
+    derivable = list(set(frozenset({-p}) for p in positive_rules if p not in answer_set) | set(
+        frozenset({n}) for n in negative_rules if n in answer_set))
     if not derivable_dict[-head]:
-        derivable_dict[-head] = answer_set_complement
+        derivable_dict[-head] = derivable
     else:
-        t_cap = []  # TODO: better name
-        for singleton in answer_set_complement:
-            for element in derivable_dict[-head]:
-                t = singleton.union(element)
-                t_cap.append(t)
-        derivable_dict[-head] = t_cap
+        derivables = []
+        for new_derivables in derivable:
+            for old_derivables in derivable_dict[-head]:
+                derivables.append(new_derivables | old_derivables)
+        derivable_dict[-head] = derivables
 
 
 def negation_atoms(program_dict: dict) -> set:
@@ -66,78 +52,127 @@ def negation_atoms(program_dict: dict) -> set:
     return nant
 
 
-def assumption_func(cautious_consequences, nant, derivable_dict, answer_set):
-    tu = set()
-    tentative_assumptions = set()
-    for atom in nant:
-        if atom not in answer_set and atom not in cautious_consequences:
-            tentative_assumptions.add(atom)
-    t, da = _derivation_path(tentative_assumptions, derivable_dict)
-    d = _dependency_assumption(da)
-    for m in d:
-        u = m.union(t)
-        tu.add({u})
-    return tu
+def _assumption_func(cautious_consequences, nant, derivable_dict, answer_set):
+    assumptions = set()
+    tentative_assumptions = set(a for a in nant if a not in answer_set and a not in cautious_consequences)
+    # print(f"[_assumption_func]: tentative_assumptions = {sorted(tentative_assumptions)}.")
+    terminals, dependency_assumptions = _derivation_path(tentative_assumptions, derivable_dict)
+    # print(f"[_assumption_func]: terminals = {terminals}")
+    # print(f"[_assumption_func]: dependency_assumptions = {dependency_assumptions}")
+    minimal_assumptions = _dependency_assumption(dependency_assumptions)
+    for minimal_assumption in minimal_assumptions:
+        assumptions.add({minimal_assumption | terminals})
+    return assumptions
 
 
 def _derivation_path(tentative_assumptions, derivable_dict):
-    da = dict()
-    t = set()
+    dependency_assumptions = dict()
+    terminals = set()
     for atom in tentative_assumptions:
-        O = tentative_assumptions.copy()
-        O.discard(atom)
-        L = derivable_dict[-atom]
-        for S in L:
-            E_a = dict()
-            E_a[-atom] = list(S)
-            _get_connection(S, derivable_dict, E_a)
-            M = set()
-            for k in E_a:
-                for V in E_a[k]:
-                    pass
-            # TODO: Untangle the paper
-    return None, None
+        tentative_destinations = tentative_assumptions.copy() - {atom}
+        safe = False
+        for derivables in derivable_dict[-atom]:
+            edge_dict = dict()
+            edge_dict[-atom] = [derivables]
+            _get_connection(derivables, derivable_dict, edge_dict)
+            paths = [{}]
+            for k in edge_dict:
+                n_paths = []
+                for neighbourhood in edge_dict[k]:
+                    for _path in paths:
+                        path = _path.copy()
+                        path[k] = neighbourhood.copy()
+                        n_paths.append(path)
+                paths = n_paths
+
+            for path in paths:
+                vertex_set = set()
+                reached_stack = []
+                cycle_dict = dict()
+                derivation_set = set()
+                safe, derivation = _check_derivation_path(-atom, path, tentative_destinations, vertex_set,
+                                                          reached_stack, cycle_dict,
+                                                          derivation_set)
+                if safe:
+                    terminals.add(atom)
+                    dependency_assumptions[atom] = derivation
+                    break  #
+            if safe:  # for the GOTO statement
+                break
+
+    return tentative_assumptions - terminals, dependency_assumptions
 
 
-def _dependency_assumption(dependency_assumption):
-    dc = set()
-    for u in dependency_assumption:
-        pass  # TODO: Untangle the paper
-    pass
+def _dependency_assumption(dependency_assumption: dict):
+    directed_cycles = set()
+    paths = [[]]
+    nexts = [list(dependency_assumption.keys())]
+    while nexts:
+        while nexts[-1]:
+            next = nexts[-1].pop()
+            path = paths[-1].copy()
+            path.append(next)
+            neighbours = []
+            for neighbour in dependency_assumption[next]:
+                if neighbour in path:
+                    if path[0] == neighbour:
+                        directed_cycles.add(frozenset(path))
+                else:
+                    neighbours.append(neighbour)
+
+            nexts.append(list(neighbour for neighbour in neighbours if neighbour not in path))
+            paths.append(path)
+
+        nexts.pop()
+        paths.pop()
+
+    return frozenset(map(frozenset, itertools.product(*directed_cycles)))
 
 
-def _get_connection(S, derivable_dict, E_a):
-    for e in S:
-        if e not in E_a and e in derivable_dict:
-            E_a[e] = derivable_dict[e]
-            for S_e in derivable_dict[e]:
-                _get_connection(S_e, derivable_dict, E_a)
+def _get_connection(derivables, derivable_dict, edge_dict):
+    for next in derivables:
+        # print(f"[_get_connection]: {next} start.")
+        if next not in edge_dict and next in derivable_dict:
+            edge_dict[next] = derivable_dict[next]
+            for next_derivables in derivable_dict[next]:
+                _get_connection(next_derivables, derivable_dict, edge_dict)  # TODO: untangle recursion
+        # print(f"[_get_connection]: {next} done.")
 
 
-def _check_derivation_path(k, N, O, V, R, C, dependency_assumptions):
-    I = N.get(k, set())
-    V.add(k)
-    R.add(k)
+def _check_derivation_path(k, path, tentative_destinations, vertex_set, reached_stack, cycle_dict,
+                           dependency_assumptions):
+    I = path.get(k, set())
+    vertex_set.add(k)
+    reached_stack.append(k)
     for i in I:
-        C[k] = i
-        j = 0 # TODO: How to calculate j?
-        if i < 0 and j in O:
+        cycle_dict[k] = i
+        j = abs(i)
+        if i < 0 and j in tentative_destinations:
             dependency_assumptions.add(j)
         else:
-            if i not in V:
-                if not _check_derivation_path(i, N, O, V, R, C, dependency_assumptions)[0]:
+            if i not in vertex_set:
+                if not \
+                        _check_derivation_path(i, path, tentative_destinations, vertex_set, reached_stack,
+                                               cycle_dict,
+                                               dependency_assumptions)[0]:
                     return False, dependency_assumptions
-            else:
-                if i in R:
-                    pass
+            elif i in reached_stack:
+                if i < 0:
+                    if not _cycle_identification(cycle_dict, i, i):
+                        return False, dependency_assumptions
+                else:
+                    return False, dependency_assumptions
 
-    return None, None
+    reached = reached_stack.pop()
+    if reached in cycle_dict:
+        del cycle_dict[reached]
+    return True, dependency_assumptions
 
 
-def _cycle_identification(C, s, e):
-    v = C[s]
+def _cycle_identification(cycle_dict, s, e):
+    v = cycle_dict[s]
     if v != e and s < 0 and v < 0:
-        return _cycle_identification(C, v, e)
+        return _cycle_identification(cycle_dict, v, e)
     return v == e and s < 0 and v < 0
 
 
@@ -145,22 +180,21 @@ def explanation_graph(atom, derivable_dict, minimal_assumption, answer_set):
     for assumed_atom in minimal_assumption:
         derivable_dict[-assumed_atom] = ["assume"]
     atom = atom if atom in answer_set else -atom
-    L = derivable_dict[atom]
-    for S in L:
+    for S in derivable_dict[atom]:
         E_a = dict()
         E_a[atom] = [S]
         _get_connection(S, derivable_dict, E_a)
         # TODO: Untangle paper
 
 
-def get_graph(k, G, N, V:set, R:list, C):
+def get_graph(k, G, N, V: set, R: list, C):
     I = N.get(k, set())
     V.add(k)
     R.append(k)
     for i in I:
         G.add(i)
         e = (k, i, 0)  # TODO: 0 should be sign, but what is sign?
-        G.add(e)       # TODO: G needs to be a graph datastructure networkx?
+        G.add(e)  # TODO: G needs to be a graph datastructure networkx?
         if i not in V:
             if not get_graph(i, G, N, V, R, C):
                 return False
@@ -176,4 +210,3 @@ def get_graph(k, G, N, V:set, R:list, C):
     if l in C:
         C.discard(l)
     return G
-
